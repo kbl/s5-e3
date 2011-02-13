9a238f86923d1f813f67ec144cf22b167f757d68
kbl <m.pietraszek@gmail.com>
2011-02-13 23:50:45 +0100

    spec test from rbtree c implementation

--- rbtree/test/test_suite.rb
+++ rbtree/test/test_suite.rb
@@ -2,5 +2,6 @@ require 'minitest/autorun'
 
 require File.expand_path(File.join(File.dirname(__FILE__), 'tree_node_test'))
 require File.expand_path(File.join(File.dirname(__FILE__), 'rb_tree_test'))
+#require File.expand_path(File.join(File.dirname(__FILE__), 'c_rbtree_impl_test'))
 
 MiniTest::Unit.autorun
