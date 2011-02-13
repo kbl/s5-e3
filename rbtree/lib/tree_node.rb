9cba51eb1572c7e011afda2eb9005f1d75b824d9
kbl <m.pietraszek@gmail.com>
2011-02-13 10:39:34 +0100

    comparable for tree_node

--- rbtree/lib/tree_node.rb
+++ rbtree/lib/tree_node.rb
@@ -2,16 +2,21 @@ require 'singleton'
 
 class TreeNode
 
+    include Comparable
+
     RED = 'red'
     BLACK = 'black'
 
     attr_reader :color
     attr_accessor :left, :right, :node_data
 
-    def initialize(left = nil, right = nil, color = BLACK)
+    def initialize(node_data = nil, color = BLACK)
         @color = color
-        @left = left
-        @right = right
+        @node_data = node_data
+    end
+
+    def <=>(other_node)
+        node_data <=> other_node.node_data
     end
 
 end
--- rbtree/test/tree_node_test.rb
+++ rbtree/test/tree_node_test.rb
@@ -14,4 +14,16 @@ class TreeNodeTest < Test::Unit::TestCase
         assert node.right == nil
     end
 
+    def test_should_properly_set_new_node_data
+        node = TreeNode.new(123)
+        assert node.node_data == 123
+    end
+
+    def test_nodes_should_inclue_comparable
+        node1 = TreeNode.new(222)
+        node2 = TreeNode.new(111)
+
+        assert (node1 <=> node2) == 1
+    end
+
 end
